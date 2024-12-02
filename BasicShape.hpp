964d351a708a4c16c89d877cd6550d109c894f90
ElliotBruhl <166664797+ElliotBruhl@users.noreply.github.com>
2024-12-02 09:05:05 -0500

    Update BasicShape.hpp

--- BasicShape.hpp
+++ BasicShape.hpp
@@ -7,19 +7,19 @@ private:
   double area;
   std::string name;
 public:
-  double getArea() {
+  double getArea() { //pre: none. post: returns area.
     return area;
   }
-  std::string getName() {
+  std::string getName() { //pre: none. post: returns name.
     return name;
   }
-  void setArea(double a) {
+  void setArea(double a) { //pre: none. post: sets area to value given.
     area = a;
   }
-  void setName(std::string n) {
+  void setName(std::string n) { //pre: none. post: sets name to value given.
     name = n;
   }
-  virtual void calcArea() = 0;
+  virtual void calcArea() = 0; //pre: must be implemented in non-abstract children. post: calculates area (specific to each shape).
 };
 
 #endif
