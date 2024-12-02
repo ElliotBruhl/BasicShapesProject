22a2f150739839db2b4241dccbdd4719df713208
ElliotBruhl <166664797+ElliotBruhl@users.noreply.github.com>
2024-12-02 09:09:57 -0500

    Update Rectangle.hpp

--- Rectangle.hpp
+++ Rectangle.hpp
@@ -7,19 +7,19 @@ private:
   double length;
   double width;
 public:
-  void calcArea() override {
+  void calcArea() override { //pre: none. post: area is calculated with A=l*w.
     setArea(length * width);
   }
-  Rectangle(double l, double w, std::string n = "Rectangle") {
+  Rectangle(double l, double w, std::string n = "Rectangle") { //pre: none. post: object is created with parameters given and area is calculated accordingly.
     length = l;
     width = w;
     setName(n);
     calcArea();
   }
-  double getLength() {
+  double getLength() { //pre: none. post: length is returned.
     return length;
   }
-  double getWidth() {
+  double getWidth() { //pre: none. post: width is returned.
     return width;
   }
 };
